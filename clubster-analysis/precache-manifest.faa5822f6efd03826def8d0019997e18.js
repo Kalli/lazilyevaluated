1871ffae1b7c87b872aa3fcb03f6fa3fc8e93956
Karl Tryggvason <ktryggvason@gmail.com>
2020-10-26 10:21:08 +0100

    Add clubster analysis

---
+++ clubster-analysis/precache-manifest.faa5822f6efd03826def8d0019997e18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6234c41cec1abf9fc016c12eb757d69c",
+    "url": "/clubster-analysis/index.html"
+  },
+  {
+    "revision": "28138d7b94c36009d1b8",
+    "url": "/clubster-analysis/static/css/main.90832af2.chunk.css"
+  },
+  {
+    "revision": "3aafdd7aa01a325658f3",
+    "url": "/clubster-analysis/static/js/2.b753c415.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clubster-analysis/static/js/2.b753c415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28138d7b94c36009d1b8",
+    "url": "/clubster-analysis/static/js/main.19ce6297.chunk.js"
+  },
+  {
+    "revision": "f0668673fa5232b76ff6",
+    "url": "/clubster-analysis/static/js/runtime-main.d9b80ec9.js"
+  },
+  {
+    "revision": "319fe11c43b73a967247a836cf1c9da4",
+    "url": "/clubster-analysis/static/media/montage.319fe11c.jpg"
+  },
+  {
+    "revision": "bc0e7744b18f4f6cfcfb174181b925c7",
+    "url": "/clubster-analysis/static/media/steps.bc0e7744.md"
+  }
+]);
